730bab898f5a5adab69bcf6bfedba098c7d9772f
jala0001 <jala0001@stud.kea.dk>
2024-05-01 14:55:09 +0200

    first commit

---
+++ src/main/java/com/example/carrentalexam/CarRentalExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carrentalexam/CarRentalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
